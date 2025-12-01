6e16f7511a1bfc02dc9af95b13721af547f2f793
ATRIKA SHOW <readeratrika08@gmail.com>
2025-12-01 11:15:49 +0530

    feat: implement username uniqueness check API endpoint

---
+++ app/api/check-unique-username/route.ts
@@ -0,0 +1,65 @@
+import dbConnect from "@/src/lib/dbConnect";
+import UserModel from "@/src/Model/User";
+import { usernameValidation } from "@/src/Schemas/signUpSchema";
+import z, { success } from "zod";
+
+const usernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+  try {
+    // get query from url
+    const { searchParams } = new URL(request.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+
+    // validate with ZOD
+    const result = usernameQuerySchema.safeParse(queryParam);
+    if (!result.success) {
+      return Response.json(
+        {
+          success: false,
+          message: "Invalid username",
+        },
+        { status: 400 }
+      );
+    }
+    const { username } = result.data;
+
+    // check in db if username exists or not
+    const existingUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 200 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Username is available",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error checking username uniqueness:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error checking username uniqueness",
+      },
+      { status: 500 }
+    );
+  }
+}
